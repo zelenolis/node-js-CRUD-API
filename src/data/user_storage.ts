ac700e4514482284d2c2a2777d8659b0b529a4d9
Eugene <josumkin@yahoo.com>
2024-02-09 13:26:27 +0400

    feat: get/post implemented

---
+++ src/data/user_storage.ts
@@ -0,0 +1,71 @@
+import { PostNewUser, User } from './types';
+import { randomUUID } from "crypto";
+
+
+class Users {
+    private userList: User[] = [];
+
+    public getAllUsers() {
+        return {
+            code: 200,
+            data: this.userList
+        }
+    }
+
+    public getCurrentUser(id: string) {
+        if (id === '' || id === ' ' || !this.uidValidation(id)) {
+            return {
+                code: 400,
+                data: `The user's Id is invalid`
+            }
+        } else if (this.userList.find(element => element.id === id)) {
+            return {
+                code: 200,
+                data: this.userList.find(element => element.id === id)
+            }
+        } else {
+            return {
+                code: 404,
+                data: `record with id = ${id} doesn't exist`
+            }
+        }
+    }
+
+    public addUser(newUser: PostNewUser) {
+        const uid = randomUUID().toString();
+        const newItem: User = {
+            id: uid,
+            username: newUser.username,
+            age: newUser.age,
+            hobbies: newUser.hobbies ? newUser.hobbies : []
+        }
+        this.userList.push(newItem);
+        return {
+            code: 201,
+            data: `user with id = ${uid} created`
+        }
+    }
+
+    public deleteUser(delId: string) {
+        if (this.userList.find(element => element.id === delId)) {
+            this.userList.filter(element => element.id !== delId);
+            return {
+                code: 204,
+                data: `user with id = ${delId} found and deleted`
+            }
+        } else {
+            return {
+                code: 404,
+                data: `user with id = ${delId} doesn\'t exists`
+            }
+        }
+    }
+
+    private uidValidation(uid: string) {
+        const uuidPattern = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+        console.log(`validation is: ${uuidPattern.test(uid)}`);
+        return uuidPattern.test(uid);
+    }
+}
+
+export const serverUsers = new Users;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,59 @@
-console.log('hello');
+import os from 'os';
+import http, { IncomingMessage } from 'http';
+import cluster from 'cluster';
+import { serverUsers } from './data/user_storage.ts';
+import { User, PostNewUser } from './data/types.ts';
+
+const PORT = 4000;
+const plainEndpointURL = '/api/users';
+const endpointURL = '/api/users/';
+const pid = process.pid;
+
+const requestListener: http.RequestListener = async (req, res) => {
+    if (req.url == plainEndpointURL && req.method === "GET") {
+        res.end(JSON.stringify(serverUsers.getAllUsers()));
+    } else if (req.url.startsWith(endpointURL) && req.method === "GET") {
+        const requestedUser = req.url.slice('/api/users/'.length);
+        res.end(JSON.stringify(serverUsers.getCurrentUser(requestedUser as string)));
+    } else if (req.method === "POST") {
+        let body = ''; 
+        req.on('data', chunk => body += chunk);
+        req.on('end', async () => {
+            try {
+                const parsedJson = await JSON.parse(body);
+                if (!parsedJson.username || !parsedJson.age || !parsedJson.hobbies) {
+                    const responseErr = {
+                        code: 400,
+                        data: '"username", "age", "hobbies" are required fields'
+                    }
+                    res.end(JSON.stringify(responseErr));
+                } else {
+                    const createNew: PostNewUser = {
+                        username: parsedJson.username || "",
+                        age: parsedJson.age || 0,
+                        hobbies: parsedJson.hobbies || []
+                    }
+                    res.end(JSON.stringify(serverUsers.addUser(createNew)));
+                }
+            } catch (error) {
+                console.error('Error parsing JSON:', error.message);
+                const responseErr = {
+                    code: 400,
+                    data: 'Invalid JSON format'
+                };
+                res.end(JSON.stringify(responseErr));
+            }
+        });
+    } else {
+        res.end({
+            code: 404,
+            data: 'The requested resource is not found'
+        });
+    }
+}
+
+const server = http.createServer(requestListener);
+
+server.listen(4000, () => {
+    console.log('Server running at http://localhost:4000');
+});
